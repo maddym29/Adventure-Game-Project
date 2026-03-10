cbcb66f7285ce03e2dc45b6c14ca206c61519f36
maddym29 <mm202935@umconnect.umt.edu>
2026-03-10 14:36:37 -0600

    Update gamefunctions.py
    Added two functions for Ass. 6 and added docstrings to complete assignment 8.

--- gamefunctions.py
+++ gamefunctions.py
@@ -1,5 +1,19 @@
+"""Use this module to calculate a purchase or create a monster.
+
+Use this module to create a game, giving your monster the ability
+to purchase items, create a menu, and welcome other players.
+"""
+
 input random
 def purchase_item(itemPrice, startingMoney, quantityToPurchase=1):
+"""
+This function calculates how many items can be purchased with a starting amount.
+Parameters: 
+    itemPrice: how much does each item cost?
+    startingMoney: how much money does your monster have?
+    quantityToPurchase: is at least one.
+Returns: none
+"""
     max_affordable = startingMoney // itemPrice
     if max_affordable < quantityToPurchase:
         items_bought = max_affordable
@@ -14,6 +28,11 @@ print(purchase_item(123, 1000, 3))
 print(purchase_item(3141, 2112))
 print(purchase_item(241, 203, 5))
 def new_random_monster():
+"""
+This function produces a monster.
+    Paramaters: none
+    Returns: none
+"""
     monsters = ["Giraffe Bear", "Smoothie Butterfly", "Kitten Dragon"]
     healthy = [3, 4, 6]
     worth = [500, 200, 400]
@@ -46,3 +65,26 @@ def new_random_monster():
         "money": money
     }     
     return monster
+def welcome(name, width):
+""" 
+This function works to welcome a player.
+    Parameters:
+        name (str): names the player
+        width (int): width of name
+    Returns: none
+"""
+    greet = print(f"Hello,{name:+^width}!")
+    return greet
+def shop_menu(item1Name, item1Price, item2Name, item2Price):
+""" This function creates a boardered menu.
+    Parameters:
+        item1Name (str): inputs first name
+        item1Price (int): inputs first price (rounds to two decimal places)
+        item2Name (str): inputs second name
+        item2Price (int): inputs second price (rounds to two decimal places)
+"""
+    line1 = print(//******************\\)
+    line2 = print(f"| {item1Name:<12}{item1Price:.2f>8}")
+    line3 = print(f"| {item2Name:<12}{item2Price:.2f>8}")
+    menu = line1 + line2 + line3 + line1
+    return menu
